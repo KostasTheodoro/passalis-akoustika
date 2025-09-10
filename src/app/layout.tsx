b74d6c8d926cc75408c4c3798d7421099f41bccc
KostasTheodoro <98785109+KostasTheodoro@users.noreply.github.com>
2025-09-10 15:31:54 +0300

    sitemap fix test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,6 @@ export default function RootLayout({
   return (
     <html lang="el">
       <head>
-        {" "}
         <meta name="msvalidate.01" content="7052D126EF24449A4DF24841FFCD2A38" />
         <Script
           id="ld-org"
